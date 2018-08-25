78dddddfcf59de33d5b73f9f5d9b8b23af6cd6de
mtdx <ms2asdk@gmail.com>
2018-08-25 05:30:07 -0700

    add axios

--- client/src/main.js
+++ client/src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 import App from './App'
 import router from './router'
+import axios from 'axios'
 
 // Vuex store
 import store from './store'
@@ -11,6 +12,11 @@ import store from './store'
 Vue.config.productionTip = false
 
 Vue.use(Vuetify)
+
+// Setup axios to be available globally through Vue
+Vue.axios = Vue.prototype.$http = axios.create({
+  baseURL: 'http://localhost:8081/api'
+})
 
 /* eslint-disable no-new */
 new Vue({
